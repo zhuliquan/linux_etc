2e5e5b4bc914a09a6fd8bd3c54eead3104136041
zhuliquan <zlqlovecode@foxmail.com>
2024-08-19 23:48:37 +0800

    Update install.sh
    install docker

--- install.sh
+++ install.sh
@@ -1,8 +1,31 @@
 #!/usr/bin/env bash
 
-# 安装git, curl, wget, openssl vim neovim ack
+# 添加阿里云源
+sudo sed -i 's/http:\/\/archive.ubuntu.com\/ubuntu/https:\/\/mirrors.aliyun.com\/ubuntu/g' /etc/apt/sources.list
+sudo sed -i 's/http:\/\/security.ubuntu.com\/ubuntu/https:\/\/mirrors.aliyun.com\/ubuntu/g' /etc/apt/sources.list
+
+# 更新源
 sudo apt-get update
-sudo apt-get install -y git curl wget openssl vim neovim ack python3-pip
+
+# 安装git, curl, wget, openssl vim neovim ack 等基础软件
+sudo apt-get install -y git curl wget openssl \
+     ack vim neovim python3-pip \
+     apt-transport-https \
+     ca-certificates gnupg lsb-release
+
+# 添加docker GPG 密钥
+curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# 添加docker镜像源
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://mirrors.aliyun.com/docker-ce/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# 更新源
+sudo apt-get update
+
+# 安装docker
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 # 安装fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
@@ -16,6 +39,10 @@ cp ./plug.vim ~/.vim/autoload/plug.vim
 cp ./.vimrc ~/.vimrc
 
 # 配置neovim config
+sudo pip3 install neovim
 mkdir -p ~/.config/nvim/
 ln -s ~/.vimrc ~/.config/nvim/init.vim
 
+# 启动docker
+sudo systemctl enable docker
+sudo systemctl start docker
