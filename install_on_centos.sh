1c274243628a386df0389fa85508562bab1cb68d
zhuliquan <zlqlovecode@foxmail.com>
2024-08-24 12:48:11 +0800

    feat: add go/rust/ndoe installer

--- install_on_centos.sh
+++ install_on_centos.sh
@@ -2,9 +2,9 @@
 
 sudo rm /etc/yum.repos.d/*.repo
 # 添加腾讯云源
-sudo cp ./Centos-Base.repo /etc/yum.repos.d/Centos-Base.repo
+sudo cp ./centos/Centos-Base.repo /etc/yum.repos.d/Centos-Base.repo
 # 添加阿里云源
-# sudo cp ./Centos-Base.repo /etc/yum.repos.d/Centos-Base.repo
+# sudo cp ./centos/Centos-Base.repo /etc/yum.repos.d/Centos-Base.repo
 
 # 更新源
 sudo yum makecache
@@ -17,11 +17,15 @@ sudo yum install -y git curl wget openssl \
      "dnf-command(config-manager)" \
 
 # 从二进制文件安装docker
-sudo bash ./install_docker.sh -s docker --mirror Aliyun
+sudo bash ./docker/install_docker.sh -s docker --mirror Aliyun
+
+# 启动docker
+sudo systemctl enable docker
+sudo systemctl start docker
 
 # 安装fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
+$HOME/.fzf/install
 
 # 安装ack
 curl https://beyondgrep.com/ack-v3.7.0 1>ack 2>/dev/null 
@@ -33,19 +37,45 @@ tar -zxf nvim-linux64.tar.gz && sudo cp nvim-linux64/bin/nvim /usr/bin/nvim
 sudo pip3 install neovim
 
 # 安装vim-plug 插件
-mkdir -p ~/.vim/{autoload,colors,doc,plugged,syntax}
-cp ./plug.vim ~/.vim/autoload/plug.vim
+mkdir -p $HOME/.vim/{autoload,colors,doc,plugged,syntax}
+cp ./plug.vim $HOME/.vim/autoload/plug.vim
 
 # 配置vimrc
-cp ./.vimrc ~/.vimrc
+cp ./vim/.vimrc $HOME/.vimrc
 
 # 配置neovim config
-mkdir -p ~/.config/nvim/
-ln -s ~/.vimrc ~/.config/nvim/init.vim
+mkdir -p $HOME/.config/nvim/
+ln -s $HOME/.vimrc $HOME/.config/nvim/init.vim
+cp ./vim/coc-settings.json $HOME/.config/nvim/coc-settings.json
 
-# 启动docker
-sudo systemctl enable docker
-sudo systemctl start docker
+# 安装node语言环境
+wget https://npmmirror.com/mirrors/node/v20.17.0/node-v20.17.0-linux-x64.tar.xz
+tar -xf node-v20.17.0-linux-x64.tar.xz
+mkdir -p $HOME/infra/
+mv node-v20.17.0-linux-x64 $HOME/infra/node
+cp ./node/.npmrc $HOME/.npmrc
+echo 'export PATH=$HOME/infra/node/bin:$PATH' >> $HOME/.bashrc
+
+# 安装go语言环境
+wget https://studygolang.com/dl/golang/go1.23.0.linux-amd64.tar.gz
+tar -zxf go1.23.0.linux-amd64.tar.gz
+mkdir -p $HOME/infra
+mv go $HOME/infra/
+mkdir -p $HOME/workspace/go/{src,pkg,bin}
+echo 'export GOROOT=$HOME/infra/go' >> $HOME/.bashrc
+echo 'export GO111MODULE=on' >> $HOME/.bashrc
+echo 'export GOPROXY=https://goproxy.io,direct' >> $HOME/.bashrc
+echo 'export GOPRIVATE=git.mycompany.com,github.com/my/private' >> $HOME/.bashrc
+echo 'export GOPATh=$HOME/workspace/go' >> $HOME/.bashrc
+echo 'export PATH=$GOROOT/bin:$GOPATH/bin:$PATH' >> $HOME/.bashrc
+
+# 安装rust语言环境
+echo 'export RUSTUP_DIST_SERVER="https://rsproxy.cn"' >> $HOME/.bashrc
+echo 'export RUSTUP_UPDATE_ROOT="https://rsproxy.cn/rustup"' >> $HOME/.bashrc
+source $HOME/.bashrc
+curl --proto '=https' --tlsv1.2 -sSf https://rsproxy.cn/rustup-init.sh | sh
+cp ./rust/config.toml $HOME/.cargo/config
+cp ./rust/config.toml $HOME/.cargo/config.toml
 
 # 启用bashrc相关配置
-source ~/.bashrc
+source $HOME/.bashrc
--- install_on_ubuntu.sh
+++ install_on_ubuntu.sh
@@ -28,25 +28,55 @@ sudo apt-get update
 # 安装docker
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
-# 安装fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-
-# 安装vim-plug 插件
-mkdir -p ~/.vim/{autoload,colors,doc,plugged,syntax}
-cp ./plug.vim ~/.vim/autoload/plug.vim
-
-# 配置vimrc
-cp ./.vimrc ~/.vimrc
-
-# 配置neovim config
-sudo pip3 install neovim
-mkdir -p ~/.config/nvim/
-ln -s ~/.vimrc ~/.config/nvim/init.vim
-
 # 启动docker
 sudo systemctl enable docker
 sudo systemctl start docker
 
+# 安装fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
+$HOME/.fzf/install
+
+# 安装vim-plug 插件
+mkdir -p $HOME/.vim/{autoload,colors,doc,plugged,syntax}
+cp ./vim/plug.vim $HOME/.vim/autoload/plug.vim
+
+# 配置vimrc
+cp ./vim/.vimrc $HOME/.vimrc
+
+# 配置neovim config
+sudo pip3 install neovim
+mkdir -p $HOME/.config/nvim/
+ln -s $HOME/.vimrc $HOME/.config/nvim/init.vim
+cp ./vim/coc-settings.json $HOME/.config/nvim/coc-settings.json
+
+# 安装node语言环境
+wget https://npmmirror.com/mirrors/node/v20.17.0/node-v20.17.0-linux-x64.tar.xz
+tar -xf node-v20.17.0-linux-x64.tar.xz
+mkdir -p $HOME/infra/
+mv node-v20.17.0-linux-x64 $HOME/infra/node
+cp ./node/.npmrc $HOME/.npmrc
+echo 'export PATH=$HOME/infra/node/bin:$PATH' >> $HOME/.bashrc
+
+# 安装go语言环境
+wget https://studygolang.com/dl/golang/go1.23.0.linux-amd64.tar.gz
+tar -zxf go1.23.0.linux-amd64.tar.gz
+mkdir -p $HOME/infra
+mv go $HOME/infra/
+mkdir -p $HOME/workspace/go/{src,pkg,bin}
+echo 'export GOROOT=$HOME/infra/go' >> $HOME/.bashrc
+echo 'export GO111MODULE=on' >> $HOME/.bashrc
+echo 'export GOPROXY=https://goproxy.io,direct' >> $HOME/.bashrc
+echo 'export GOPRIVATE=git.mycompany.com,github.com/my/private' >> $HOME/.bashrc
+echo 'export GOPATh=$HOME/workspace/go' >> $HOME/.bashrc
+echo 'export PATH=$GOROOT/bin:$GOPATH/bin:$PATH' >> $HOME/.bashrc
+
+# 安装rust语言环境
+echo 'export RUSTUP_DIST_SERVER="https://rsproxy.cn"' >> $HOME/.bashrc
+echo 'export RUSTUP_UPDATE_ROOT="https://rsproxy.cn/rustup"' >> $HOME/.bashrc
+source $HOME/.bashrc
+curl --proto '=https' --tlsv1.2 -sSf https://rsproxy.cn/rustup-init.sh | sh
+cp ./rust/config.toml $HOME/.cargo/config
+cp ./rust/config.toml $HOME/.cargo/config.toml
+
 # 启用bashrc相关配置
-source ~/.bashrc
+source $HOME/.bashrc
